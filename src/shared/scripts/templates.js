f7c3661beffc665f8f9d204c62fa584c9a0cad71
Winkelmann, Felix <felix.winkelmann@aperto.com>
2017-12-04 23:20:39 +0100

    add component styles

--- src/shared/scripts/templates.js
+++ src/shared/scripts/templates.js
@@ -199,20 +199,44 @@ window["App"]["Templates"]["SCROLLITEM"] = Handlebars.template({"1":function(con
   return "\n	 data-js-options='"
     + container.escapeExpression((helpers.stringify || (depth0 && depth0.stringify) || helpers.helperMissing).call(depth0 != null ? depth0 : (container.nullContext || {}),((stack1 = (depth0 != null ? depth0.settings : depth0)) != null ? stack1.scrollItemJsOptions : stack1),{"name":"stringify","hash":{},"data":data}))
     + "'";
+},"9":function(container,depth0,helpers,partials,data) {
+    var stack1, alias1=container.escapeExpression;
+
+  return "			<div class=\"scroll-item__imagewrapper\">\n				<img class=\"scroll-item__image\" src=\""
+    + alias1((helpers.getUrl || (depth0 && depth0.getUrl) || helpers.helperMissing).call(depth0 != null ? depth0 : (container.nullContext || {}),((stack1 = ((stack1 = (depth0 != null ? depth0.content : depth0)) != null ? stack1.img : stack1)) != null ? stack1.src : stack1),{"name":"getUrl","hash":{},"data":data}))
+    + "\" alt=\""
+    + alias1(container.lambda(((stack1 = ((stack1 = (depth0 != null ? depth0.content : depth0)) != null ? stack1.img : stack1)) != null ? stack1.alt : stack1), depth0))
+    + "\"/>\n			</div>\n";
+},"11":function(container,depth0,helpers,partials,data) {
+    return "				<h2 class=\"scroll-item__headline\"></h2>\n";
+},"13":function(container,depth0,helpers,partials,data) {
+    var stack1;
+
+  return "				<ul class=\"scroll-item__keywords\">\n"
+    + ((stack1 = helpers.each.call(depth0 != null ? depth0 : (container.nullContext || {}),((stack1 = (depth0 != null ? depth0.content : depth0)) != null ? stack1.keywords : stack1),{"name":"each","hash":{},"fn":container.program(14, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + "				</ul>\n";
+},"14":function(container,depth0,helpers,partials,data) {
+    return "						<li class=\"scroll-item__keywords-item\">\n							"
+    + container.escapeExpression(container.lambda(depth0, depth0))
+    + "\n						</li>\n";
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    var stack1, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=container.lambda, alias3=container.escapeExpression;
+    var stack1, alias1=depth0 != null ? depth0 : (container.nullContext || {});
 
   return "<div class=\"c-scroll-item"
     + ((stack1 = helpers["if"].call(alias1,((stack1 = (depth0 != null ? depth0.settings : depth0)) != null ? stack1.scrollItemContextClass : stack1),{"name":"if","hash":{},"fn":container.program(1, data, 0),"inverse":container.program(3, data, 0),"data":data})) != null ? stack1 : "")
     + " section"
     + ((stack1 = helpers["if"].call(alias1,((stack1 = (depth0 != null ? depth0.settings : depth0)) != null ? stack1.scrollItemClasses : stack1),{"name":"if","hash":{},"fn":container.program(5, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
     + "\"\n	 id=\""
-    + alias3(alias2(((stack1 = (depth0 != null ? depth0.settings : depth0)) != null ? stack1.scrollItemID : stack1), depth0))
+    + container.escapeExpression(container.lambda(((stack1 = (depth0 != null ? depth0.settings : depth0)) != null ? stack1.scrollItemID : stack1), depth0))
     + "\"\n	 data-css=\"c-scroll-item\"\n	 data-js-item=\"scroll-item\"\n	 data-js-module=\"scroll-item\""
     + ((stack1 = helpers["if"].call(alias1,((stack1 = (depth0 != null ? depth0.settings : depth0)) != null ? stack1.scrollItemJsOptions : stack1),{"name":"if","hash":{},"fn":container.program(7, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
-    + ">\n\n	<div class=\"scroll-item__bg\"></div>\n\n	"
-    + alias3(alias2(((stack1 = (depth0 != null ? depth0.docs : depth0)) != null ? stack1.variationName : stack1), depth0))
-    + "\n</div>\n";
+    + ">\n\n	<div class=\"scroll-item__bg\"></div>\n\n	<div class=\"scroll-item__header\">\n"
+    + ((stack1 = helpers["if"].call(alias1,((stack1 = (depth0 != null ? depth0.content : depth0)) != null ? stack1.img : stack1),{"name":"if","hash":{},"fn":container.program(9, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + "\n		<div class=\"scroll-item__textwrapper\">\n"
+    + ((stack1 = helpers["if"].call(alias1,((stack1 = (depth0 != null ? depth0.content : depth0)) != null ? stack1.headline : stack1),{"name":"if","hash":{},"fn":container.program(11, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + "\n"
+    + ((stack1 = helpers["if"].call(alias1,((stack1 = (depth0 != null ? depth0.content : depth0)) != null ? stack1.keywords : stack1),{"name":"if","hash":{},"fn":container.program(13, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + "		</div>\n	</div>\n\n</div>\n";
 },"useData":true});
 
 return window["App"]["Templates"];
