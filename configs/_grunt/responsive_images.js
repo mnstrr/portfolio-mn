0c67887fca913ef741c274cd26d040262003d143
Winkelmann, Felix <felix.winkelmann@aperto.com>
2017-12-17 00:12:16 +0100

    add siemens case content

--- configs/_grunt/responsive_images.js
+++ configs/_grunt/responsive_images.js
@@ -115,5 +115,44 @@ module.exports = {
 				dest: '<%= paths.src %>/assets/media/images/png/case-magine-b'
 			}
 		]
+	},
+	"caseSiemens": {
+		options: {
+			sizes: [
+				{
+					name: '480',
+					width: 480,
+					quality: 80
+				},
+				{
+					name: "960",
+					width: 960,
+					quality: 80
+				},
+				{
+					name: "1200",
+					width: 1200,
+					quality: 80
+				},
+				{
+					name: "1600",
+					width: 1600,
+					quality: 80
+				},
+				{
+					name: "2000",
+					width: 2000,
+					quality: 80
+				}
+			]
+		},
+		files: [
+			{
+				expand: true,
+				src: ['**.{jpg,gif,png}'],
+				cwd: '<%= paths.src %>/shared/assets/img/png-src/case-siemens',
+				dest: '<%= paths.src %>/assets/media/images/png/case-siemens'
+			}
+		]
 	}
 };
---
+++ src/shared/components/case-siemens/INSERTPOINTS.md
@@ -0,0 +1,12 @@
+
+## INSERTPOINTS
+
+### Include: Page
+
+``` hbs
+{{! @INSERT :: START @id: case-siemens, @tag: component-partial }}
+{{#with case-siemens}}
+	{{> case-siemens}}
+{{/with}}
+{{! @INSERT :: END }}
+```
